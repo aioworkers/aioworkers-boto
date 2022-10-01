049f932da31e17e8f5290bbdcd546e3a2ef98911
Alexander Malev <malev@somedev.ru>
2022-10-01 18:31:02 +0300

    improve storage

--- aioworkers_boto/storage.py
+++ aioworkers_boto/storage.py
@@ -1,5 +1,10 @@
+import os
+from pathlib import PurePath
+from typing import Any
+
 from aioworkers.core.formatter import FormattedEntity
 from aioworkers.storage.base import AbstractStorage
+from aioworkers.storage.filesystem import flat
 
 from aioworkers_boto.core import Connector
 
@@ -7,30 +12,41 @@ from aioworkers_boto.core import Connector
 class Storage(AbstractStorage, FormattedEntity, Connector):
     def __init__(self, *args, **kwargs):
         kwargs["service_name"] = "s3"
+        self._path = kwargs.get("path", ".")
+        self._bucket = kwargs.get("bucket", "")
         super().__init__(*args, **kwargs)
 
-    def raw_key(self, key):
-        path = self.config.get("path")
-        if path:
-            key = f"{path}/{key}"
-        return key
+    def set_config(self, config) -> None:
+        super().set_config(config)
+        self._path = self.config.get("path", self._path)
+        self._bucket = self.config.get("bucket", self._bucket)
 
-    async def get(self, key):
-        kwargs = {"Key": self.raw_key(key)}
-        bucket = self.config.get("bucket")
+    def raw_key(self, *key) -> PurePath:
+        path = os.path.join(self._path, *flat(key))
+        result = PurePath(os.path.normpath(path))
+        result.relative_to(self._path)
+        return result
+
+    async def get(self, key: str, *, bucket: str = "") -> Any:
+        kwargs = {"Key": str(self.raw_key(key))}
+        bucket = bucket or self._bucket
         if bucket:
             kwargs["Bucket"] = bucket
-        response = await self._client.get_object(**kwargs)
+        else:
+            raise RuntimeError("Not allowed empty bucket")
+        response = await self.client.get_object(**kwargs)
         async with response["Body"] as stream:
             return self.decode(await stream.read())
 
-    async def set(self, key, value):
-        kwargs = {"Key": self.raw_key(key)}
-        bucket = self.config.get("bucket")
+    async def set(self, key: str, value: Any, *, bucket: str = ""):
+        kwargs = {"Key": str(self.raw_key(key))}
+        bucket = bucket or self._bucket
         if bucket:
             kwargs["Bucket"] = bucket
+        else:
+            raise RuntimeError("Not allowed empty bucket")
         if value is None:
-            await self._client.delete_object(**kwargs)
+            await self.client.delete_object(**kwargs)
         else:
             kwargs["Body"] = self.encode(value)
-            await self._client.put_object(**kwargs)
+            await self.client.put_object(**kwargs)
--- tests/test_storage.py
+++ tests/test_storage.py
@@ -1,10 +1,9 @@
 from unittest.mock import MagicMock
 
 import pytest
+from aioworkers.core.config import ValueExtractor
 
 from aioworkers_boto.storage import Storage
-
-pytestmark = pytest.mark.asyncio
 
 
 async def mocked_client_factory(data):
@@ -33,13 +32,13 @@ async def mocked_client_factory(data):
 @pytest.mark.parametrize(
     "kw,data",
     [
-        (dict(format='json'), {'a': 'b'}),
-        (dict(bucket="b", path="p"), b"1234"),
+        (dict(format="json"), {"a": "b"}),
+        (dict(path="p"), b"1234"),
         (dict(), "1234"),
     ],
 )
 async def test_set_get(mocker, kw, data):
-    s3 = Storage(**kw)
+    s3 = Storage(bucket="b", **kw)
     mocked_client = await mocked_client_factory(s3.encode(data))
     mocker.patch.object(s3, "_client", mocked_client)
 
@@ -48,3 +47,61 @@ async def test_set_get(mocker, kw, data):
     d = await s3.get(key)
     await s3.set(key, None)
     assert d == data
+
+
+async def test_set_get_bucket(mocker):
+    s3 = Storage()
+    bucket = "123"
+    data = b"123"
+    mocked_client = await mocked_client_factory(s3.encode(data))
+    mocker.patch.object(s3, "_client", mocked_client)
+
+    key = "1"
+    await s3.set(key, data, bucket=bucket)
+    d = await s3.get(key, bucket=bucket)
+    await s3.set(key, None, bucket=bucket)
+    assert d == data
+
+
+async def test_empty_bucket_set():
+    with pytest.raises(RuntimeError):
+        await Storage().set("a", "")
+
+
+async def test_empty_bucket_get():
+    with pytest.raises(RuntimeError):
+        await Storage().get("a")
+
+
+@pytest.mark.parametrize(
+    "path,key,result",
+    [
+        (".", "b", "b"),
+        ("/a", "b", "/a/b"),
+        ("a", "b", "a/b"),
+        ("a/c", "b", "a/c/b"),
+    ],
+)
+def test_raw_key(path, key, result):
+    s = Storage(path=path)
+    assert str(s.raw_key(key)) == result
+
+
+@pytest.mark.parametrize(
+    "path,key",
+    [
+        ("a/c", "../b"),
+        ("a/b/c", "../b"),
+    ],
+)
+def test_bad_key(path, key):
+    s = Storage(path=path)
+    with pytest.raises(ValueError):
+        assert s.raw_key(key)
+
+
+def test_set_cfg():
+    s = Storage(path="a")
+    assert s._path == "a"
+    s.set_config(ValueExtractor(dict(path="b", name="n")))
+    assert s._path == "b"
