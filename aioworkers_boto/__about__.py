d79ebc6488d260b98779049f1b2868842f631dc4
Alexander Malev <malev@somedev.ru>
2023-05-09 23:02:20 +0300

    __version__ from __init__

--- aioworkers_boto/__about__.py
+++
@@ -1,4 +0,0 @@
-# SPDX-FileCopyrightText: 2022-present Alexander Malev <malev@somedev.ru>
-#
-# SPDX-License-Identifier: Apache-2.0
-__version__ = "0.0.0"
--- aioworkers_boto/__init__.py
+++ aioworkers_boto/__init__.py
@@ -1,3 +1,1 @@
-# SPDX-FileCopyrightText: 2022-present Alexander Malev <malev@somedev.ru>
-#
-# SPDX-License-Identifier: Apache-2.0
+__version__ = "0.0.0"
